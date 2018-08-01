0e7025fa71709739e4d7804ef4d793929046b35d
AntoineLu <39118793+AntoineLu@users.noreply.github.com>
2018-08-01 10:15:31 +0200

    Delete nonlinearleastsq.m

--- Matlab5/nonlinearleastsq.m
+++
@@ -1,5 +0,0 @@
-function newParameters = nonlinearleastsq(parameters,stepSize,exptX,exptY,n,fonction)
-%Calcul de la matrice Hessienne
-[hessB,Hess] = Hessian(parameters,stepSize,exptX,exptY,n,fonction);
-newParameters = parameters + Hess\hessB'; %Nouveaux paramètres A
-end
