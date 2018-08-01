713fe2ecd00692f639fc31c87ade29211a8aa6fa
AntoineLu <39118793+AntoineLu@users.noreply.github.com>
2018-08-01 10:15:37 +0200

    Delete sumsqerror.m

--- Matlab5/sumsqerror.m
+++
@@ -1,7 +0,0 @@
-function f=sumsqerror(parameters,exptX,exptY,n,fonction)
-%theoY = FittingFunction2(exptX,parameters);
-%theoY = fonction(exptX,parameters,n);
-theoY = arrayfun(@(i)fonction(i,parameters,n),exptX);
-b=(exptY-theoY).^2;
-f=sum(b);
-end
